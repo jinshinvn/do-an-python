df4d352ab0413a6ade1682d10e7b22210462455d
jinshinvn <thinhnguyen.280202@gmail.com>
2022-05-03 10:30:45 +0700

    update 2 form thêm hàng

--- test1.py
+++ test1.py
@@ -1,13 +1,28 @@
-from matplotlib import pyplot as plt
-def cvPicPlot(str):
-    print(str)
-    plt.plot()
-    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    y = [10, 20, 30, 40, 50, 60, 10, 20, 30, 11, 11, 12]
-    plt.plot(x, y, color = "red"); #put ; to remove [<matplotlib.lines.Line2D at 0x17859473280>]
-    plt.xlabel('MONTH')
-    plt.ylabel('USD')
-    plt.tight_layout()
-    # plt.savefig('plot.png')
-    plt.title('Doanh thu theo tháng tính bằng USD')
-cvPicPlot("Xin chào")
+from tkinter import *
+from tkcalendar import Calendar
+ 
+# Create Object
+root = Tk()
+ 
+# Set geometry
+root.geometry("400x400")
+ 
+# Add Calendar
+cal = Calendar(root, selectmode = 'day',
+               year = 2020, month = 5,
+               day = 22)
+ 
+cal.pack(pady = 20)
+ 
+def grad_date():
+    date.config(text = "Selected Date is: " + cal.get_date())
+ 
+# Add Button and Label
+Button(root, text = "Get Date",
+       command = grad_date).pack(pady = 20)
+ 
+date = Label(root, text = "")
+date.pack(pady = 20)
+ 
+# Execute Tkinter
+root.mainloop()
